8aabcac7d59c9bc9ca77f6d63435d119439d029d
Osvaldo <ofarinas12@gmail.com>
2015-03-14 17:57:05 +0000

    no message

--- test/InterpreterTest.java
+++ test/InterpreterTest.java
@@ -15,7 +15,7 @@ import static junit.framework.Assert.*;
  */
 public class InterpreterTest {
     @Test
-    public void mock_integration_test_whith_One_Number() throws Exception {
+    public void mock_integration_test_with_One_Number() throws Exception {
         String expression = "1";
         Parser parser= expression1 -> new Constant(1);
         Object result = Evaluator.evaluate((Expression) parser.parser(expression));
